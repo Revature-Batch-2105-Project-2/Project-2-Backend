247193cf54202ee447693a16f4af62f4dec5a648
shadekiller666 <ezioauditorerevs@gmail.com>
2021-07-15 18:22:50 -0700

    Added a bunch of endpoints for accessing Work Orders.

--- src/main/java/com/revature/repo/IWorkOrderRepo.java
+++ src/main/java/com/revature/repo/IWorkOrderRepo.java
@@ -1,5 +1,8 @@
 package com.revature.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,12 @@ import com.revature.beans.WorkOrder;
 
 @Repository
 public interface IWorkOrderRepo extends CrudRepository<WorkOrder, Integer>{
-
+	List<WorkOrder> findByVehicle_Id(int id);
+	
+	@Query("from WorkOrder as w left outer join w.vehicle as v left outer join v.customer as c where c.id = ?1")
+	List<WorkOrder> findByCustomerId(int id);
+	
+	List<WorkOrder> findByVehicleCustomerEmail(String email);
+	List<WorkOrder> findByVehicleCustomerPhoneNumber(String phone);
+	List<WorkOrder> findByVehicleCustomerFirstNameAndVehicleCustomerLastName(String firstName, String lastName);
 }
--- src/main/java/com/revature/services/WorkOrderService.java
+++ src/main/java/com/revature/services/WorkOrderService.java
@@ -21,6 +21,31 @@ public class WorkOrderService implements IWorkOrderService {
 	public WorkOrder get(int id) {
 		return this.repo.findById(id).get();
 	}
+	
+	@Override
+	public List<WorkOrder> getByVehicleId(int id) {
+		return this.repo.findByVehicle_Id(id);
+	}
+	
+	@Override
+	public List<WorkOrder> getByCustomerId(int id) {
+		return this.repo.findByCustomerId(id);
+	}
+	
+	@Override
+	public List<WorkOrder> getByCustomerEmail(String email) {
+		return this.repo.findByVehicleCustomerEmail(email);
+	}
+	
+	@Override
+	public List<WorkOrder> getByCustomerPhoneNumber(String phone) {
+		return this.repo.findByVehicleCustomerPhoneNumber(phone);
+	}
+	
+	@Override
+	public List<WorkOrder> getByCustomerName(String firstName, String lastName) {
+		return this.repo.findByVehicleCustomerFirstNameAndVehicleCustomerLastName(firstName, lastName);
+	}
 
 	@Override
 	public List<WorkOrder> getAll() {
